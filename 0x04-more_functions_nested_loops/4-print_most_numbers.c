1b61247352781ba38664d8f9398a84d4b0dfb0b6
Mintesinotalex <mintesinotalemayehu7@gmail.com>
2022-07-07 14:36:57 -0700

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
@@ -17,5 +17,5 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
-	/_putchar('\n');
+	_putchar('\n');
 }
